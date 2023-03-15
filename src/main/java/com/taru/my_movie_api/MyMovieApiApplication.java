f92e9b305f261395b3d6e112fbe6ddf0ede3cad8
Timur Turianytsia <turianytsia.timur@gmail.com>
2023-03-15 11:04:22 +0200

    Working Project
    Connected to DB

---
+++ src/main/java/com/taru/my_movie_api/MyMovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.taru.my_movie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taru/my_movie_api/MyMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taru.my_movie_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
