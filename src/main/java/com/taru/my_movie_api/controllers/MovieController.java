41a929f11f4208ad1f471ae0549998db34d70dfe
Timur Turianytsia <turianytsia.timur@gmail.com>
2023-03-15 20:49:00 +0200

    feat: Add Pagination for Movie object
    Added pagination for getAllMovies

--- src/main/java/com/taru/my_movie_api/controllers/MovieController.java
+++ src/main/java/com/taru/my_movie_api/controllers/MovieController.java
@@ -1,5 +1,6 @@
 package com.taru.my_movie_api.controllers;
 
+import com.taru.my_movie_api.dto.MovieResponse;
 import com.taru.my_movie_api.models.Movie;
 import com.taru.my_movie_api.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,7 +24,10 @@ public class MovieController {
     }
 
     @GetMapping("movie")
-    public ResponseEntity<List<Movie>> getMovies(){
-        return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
+    public ResponseEntity<MovieResponse> getMovies(
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
+    ){
+        return new ResponseEntity<>(movieService.getAllMovies(pageNo,pageSize), HttpStatus.OK);
     }
 }
